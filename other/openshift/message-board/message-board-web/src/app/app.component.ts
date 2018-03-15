7e47d643358d688b77c513537056471b47be6f88
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-15 15:59:58 +0100

    creating components and add style (bootstrap)

--- other/openshift/message-board/message-board-web/src/app/app.component.ts
+++ other/openshift/message-board/message-board-web/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Message Board';
--- other/openshift/message-board/message-board-web/src/app/app.module.ts
+++ other/openshift/message-board/message-board-web/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { LoginService } from './login.service';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BoardDetailComponent } from './board-detail/board-detail.component';
+import { BoardListComponent } from './board-list/board-list.component';
+import { PostMessageComponent } from './post-message/post-message.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SigninComponent
+    SigninComponent,
+    BoardDetailComponent,
+    BoardListComponent,
+    PostMessageComponent
   ],
   imports: [
     BrowserModule,
