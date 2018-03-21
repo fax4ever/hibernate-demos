343b3ceb1bca9a4c7f180917af4ac52ad6d95a7d
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-21 11:26:01 +0100

    create script for OCP binary build

---
+++ other/openshift/message-board/script/create-binary-build-directory.sh
@@ -0,0 +1,10 @@
+rm -fr account-service
+rm -fr message-service
+rm -fr nocontent
+mkdir account-service
+mkdir message-service
+mkdir nocontent
+cp ../account-service/target/account-service.war ./account-service
+cp ../message-service/target/message-service.war ./message-service
+cp -r ../message-service/target/wildfly-11.0.0.Final/modules ./message-service/modules
+rm -fr ./message-service/modules/system
