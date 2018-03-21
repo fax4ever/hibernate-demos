59394fbb7587a97c022a8a53420d16c27833273a
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-21 15:40:28 +0100

    adding MySql datasource

--- other/openshift/message-board/script/install-all.sh
+++ other/openshift/message-board/script/install-all.sh
@@ -13,16 +13,16 @@ oc create -f ../template/infinispan-persistent.json
 oc new-app --template=infinispan-persistent
 
 # Install MySql Server
-oc new-app --template=mysql-persistent -e MYSQL_USER=messageboard MYSQL_PASSWORD=redhat MYSQL_ROOT_PASSWORD=hibernate MYSQL_DATABASE=account
+oc new-app --template=mysql-persistent -p DATABASE_SERVICE_NAME=account-mysql -p MYSQL_USER=messageboard -p MYSQL_PASSWORD=redhat -p MYSQL_ROOT_PASSWORD=hibernate MYSQL_DATABASE=account --name=account-mysql
 
 # Install Message MicroService
 oc import-image my-jboss-eap-7/eap71-openshift --from=registry.access.redhat.com/jboss-eap-7/eap71-openshift --confirm
-oc new-app --image-stream=eap71-openshift:latest~./nocontent --name=message-service
+oc new-app --image-stream=eap71-openshift:latest~./nocontent -e OPENSHIFT_KUBE_PING_NAMESPACE=message --name=message-service
 oc start-build message-service --from-dir=./message-service --follow
 oc expose svc/message-service
 
 # Install Account MicroService
-oc new-app --image-stream=eap71-openshift:latest~./nocontent --name=account-service
+oc new-app --image-stream=eap71-openshift:latest~./nocontent -e DB_SERVICE_PREFIX_MAPPING="account-mysql=MYSQL" -e MYSQL_USERNAME=messageboard -e MYSQL_PASSWORD=redhat -e MYSQL_DATABASE=account -e OPENSHIFT_KUBE_PING_NAMESPACE=account --name=account-service
 oc start-build account-service --from-dir=./account-service --follow
 oc expose svc/account-service
 
