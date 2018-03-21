1854d968bb6c2df11adaac4e60ddf68daa80800d
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-21 14:20:49 +0100

    fix exception handler. The root exception on EJB is always an EjbException.

--- other/openshift/message-board/account-service/src/main/java/org/hibernate/demo/message/account/core/service/UserService.java
+++ other/openshift/message-board/account-service/src/main/java/org/hibernate/demo/message/account/core/service/UserService.java
@@ -13,8 +13,8 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.hibernate.demo.message.account.core.entity.User;
@@ -26,6 +26,7 @@ import org.hibernate.demo.message.account.core.repo.UserRepo;
 @Path("/user")
 @Stateless
 public class UserService {
+
 	@Inject
 	private UserRepo repo;
 
@@ -36,8 +37,9 @@ public class UserService {
 	}
 
 	@GET
+	@Path( "{username}" )
 	@Produces(MediaType.APPLICATION_JSON)
-	public User findByUsername(@QueryParam("username") String username) {
+	public User findByUsername(@PathParam("username") String username) {
 		return repo.findByUserName( username );
 	}
 
--- other/openshift/message-board/account-service/src/main/java/org/hibernate/demo/message/account/core/service/exception/EjbExceptionMapper.java
+++ other/openshift/message-board/account-service/src/main/java/org/hibernate/demo/message/account/core/service/exception/EjbExceptionMapper.java
@@ -1,19 +1,24 @@
 package org.hibernate.demo.message.account.core.service.exception;
 
+import javax.ejb.EJBException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import org.hibernate.HibernateException;
-
 @Provider
-public class HibernateExceptionMapper implements ExceptionMapper<HibernateException> {
+public class EjbExceptionMapper implements ExceptionMapper<EJBException> {
 
 	@Override
-	public Response toResponse(HibernateException e) {
+	public Response toResponse(EJBException e) {
+
+		Throwable cause = e.getCause();
+		if ( cause == null ) {
+			cause = e;
+		}
+
 		return Response.status( 500 )
-			.entity( new ExceptionMessage( 123, e.getClass().toString(), e.getMessage() ) )
+			.entity( new ExceptionMessage( 234, cause.getClass().toString(), cause.getMessage() ) )
 			.type( MediaType.APPLICATION_JSON )
 			.build();
 	}
