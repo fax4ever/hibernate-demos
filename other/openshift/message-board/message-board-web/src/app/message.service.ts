0255a8a97c4647a4ad3913b693d4bdf2ac35da8e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-26 11:18:30 +0200

    add post message

--- other/openshift/message-board/message-board-web/src/app/message.service.ts
+++ other/openshift/message-board/message-board-web/src/app/message.service.ts
@@ -6,8 +6,14 @@ import { of } from 'rxjs/observable/of';
 
 import { Message } from './message';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable()
 export class MessageService {
+
+  private username: string;
 
   constructor( private http: HttpClient ) { }
 
@@ -16,7 +22,15 @@ export class MessageService {
       // if not search term, return empty message array.
       return of([]);
     }
+    this.username = term;
     return this.http.get<Message[]>(`message-service/messages?username=${term}`);
+  }
+
+  postMessage(body : string): Observable<any> {
+    var message = new Message();
+    message.username = this.username
+    message.body = body;
+    return this.http.post<Message>("message-service/messages", message, httpOptions);
   }
 
 }
--- other/openshift/message-board/message-board-web/src/app/post-message/post-message.component.ts
+++ other/openshift/message-board/message-board-web/src/app/post-message/post-message.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MessageService }  from '../message.service';
+import { BoardDetailComponent } from '../board-detail/board-detail.component';
 
 @Component({
   selector: 'app-post-message',
-  templateUrl: './post-message.component.html',
-  styleUrls: ['./post-message.component.css']
+  templateUrl: './post-message.component.html'
 })
 export class PostMessageComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  text: string;
+
+  constructor( private service: MessageService, private messages: BoardDetailComponent ) { }
 
   ngOnInit() {
+  }
+
+  postMessage(): void {
+    console.log("Posting message:" + this.text);
+    this.service.postMessage(this.text)
+      .subscribe(result => {
+        this.messages.getMessages();
+      })
+    this.text = "";
   }
 
 }
